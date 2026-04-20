a6fd7407ad8d3af926dfdc98098391542388626b
AbhayManmode <kmaproperty22@gmail.com>
2026-04-20 13:53:58 +0530

    fix

--- buyer/src/components/channelParterner/mainLayout.tsx
+++ buyer/src/components/channelParterner/mainLayout.tsx
@@ -18,7 +18,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
         </div>
         <div className="absolute top-0 left-0 w-full h-[450px] bg-blue rounded-b-[25px] sm:rounded-b-[60px] lg:rounded-b-[80px] xl:rounded-b-[100px]" />
 
-        <div className="flex justify-center pt-[6rem] md:pt-[8.5rem] pb-[2rem] relative z-1 w-full">
+        <div className="flex justify-center pt-[10rem] md:pt-[8.5rem] pb-[2rem] relative z-1 w-full">
           <div className="w-[90%] xl:w-[75%] min-h-[66dvh] flex">
             {children}
           </div>
