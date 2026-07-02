18d89d6f5d71fbc88f7fe193f088c040ce22515d
kma-global-property <abhishekhnandal575@gmail.com>
2026-07-02 17:03:56 +0530

    Prevent API call from cache

--- seller/src/app/api/verify-image/route.ts
+++ seller/src/app/api/verify-image/route.ts
@@ -172,6 +172,9 @@
 //   }
 // }
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import { NextRequest, NextResponse } from "next/server";
 import { v2 as cloudinary } from "cloudinary";
 import axios from "axios";
