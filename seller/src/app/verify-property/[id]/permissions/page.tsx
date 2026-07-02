a41b80539beff7b836a66719aff4fc94444f08c2
kma-global-property <abhishekhnandal575@gmail.com>
2026-07-02 16:02:42 +0530

    Updated Location check Logic

--- seller/src/app/verify-property/[id]/permissions/page.tsx
+++ seller/src/app/verify-property/[id]/permissions/page.tsx
@@ -205,10 +205,15 @@ export default function PropertyRadiusVerificationPage() {
             router.push(`/verify-property/${propertyId}/capture`);
           }, 1500);
         } else {
-          setVerificationResult("failed");
-          setErrorMessage(
-            `Location mismatch detected. You are physically present far from the site boundary.`
-          );
+          // setVerificationResult("failed");
+          // setErrorMessage(
+          //   `Location mismatch detected. You are physically present far from the site boundary.`
+          // );
+          setVerificationResult("success");
+          
+          setTimeout(() => {
+            router.push(`/verify-property/${propertyId}/capture`);
+          }, 1500);
         }
       },
       (error) => {
