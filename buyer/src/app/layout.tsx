c656255ab0cc1d792eff82b35c5d87976e49d381
kma-global-property <abhishekhnandal575@gmail.com>
2026-07-08 14:37:30 +0530

    Added Meta pixel code

--- buyer/src/app/layout.tsx
+++ buyer/src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Suspense } from "react";
 import TopLoaderProvider from "@/providers/TopLoaderProvider";
 import CookieSessionGuard from "@/components/common/cookieSessionGuard";
 import WhatsAppSticky from "@/components/whatsappSticky/WhatsAppSticky";
+import Script from "next/script";
 
 const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -39,10 +40,33 @@ export default function RootLayout({
             __html: gtmScript,
           }}
         />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3837781979695365');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body
         className={`${ibmPlexSans.variable} antialiased min-w-0 overflow-x-hidden`}
       >
+      <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=3837781979695365&ev=PageView&noscript=1"
+            alt="meta-pixel-fallback"
+          />
+        </noscript>
         <script
           dangerouslySetInnerHTML={{
             __html: gtmScript,
